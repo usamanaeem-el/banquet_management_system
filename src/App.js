a94b10edccbfe94558d5f6f2c126c43e86de6208
personalGit <usamamanghat917@gmail.com>
2023-08-05 10:41:05 +0500

    configure tailwind css in project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Landing page22</h1>
+      <h1 className="text-gray-400">Landing page22</h1>
     </div>
   );
 }
